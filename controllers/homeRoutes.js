0f1f7a71ec7cac0ae58564b8d856fd4f166b148c
Hope <johnhope354@gmail.com>
2021-07-24 13:16:05 -0500

    finished conversion to handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,20 +16,4 @@ router.get('/characterChoice', async (req, res) => {
   }
 });
 
-// Render the login page
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-});
-  
-// Render the signup page
-router.get("/signup", async (req, res) => {
-  res.render("signup");
-});
-
 module.exports = router;
