41b93c42de8cca9a4c316edeb21728692fcd4fd4
Daniel Klauer <danielkl12@icloud.com>
2021-07-24 11:46:20 -0500

    updated signup.js file

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,22 @@
 (() => {
     const signUpForm = $("form.signup");
-    const emailInput = $("input#email-input");
     const usernameInput = $("input#username-input");
     const passwordInput = $("input#password-input");
     
     signUpForm.on("submit", (event) => {
         event.preventDefault();
         const userData = {
-            email: emailInput.val().trim(),
+            userName: usernameInput.val().trim(),
             passwordInput: passwordInput.val().trim()
         };
-        if (!userData.email || userData.password) return;
-        signUpUser(userData.email, userData.password);
-        emailInput.val("");
-            username: usernameInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
+        // if (!userData.userName || userData.password) return;
+        // newUserData(userData.userName, userData.password);
+        // usernameInput.val("");
+        //   //  userName: usernameInput.val().trim(),
+        //   //  passwordInput: passwordInput.val().trim()
+        
         if (!userData.username || userData.password) return;
-        signUpUser(userData.username, userData.password);
+        newUserData(userData.username, userData.password);
         usernameInput.val("");
 
         passwordInput.val("");
@@ -25,11 +24,8 @@
 
     });
 
-    function signUpUser(email, password) {
-        $.post("/signup", {
-            email: email,
-
-    function signUpUser(username, password) {
+ 
+    function newUserData(username, password) {
         $.post("/signup", {
             username: username,
 
@@ -43,4 +39,4 @@
         });
         
     }
-})
+});
