0959815d46f5534b1a3b73151afef7d863b92bbf
Hope <johnhope354@gmail.com>
2021-07-26 19:43:37 -0500

    deleted HTML

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,6 +8,14 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/characterChoice', async (req, res) => {
   try {
     res.render('characterChoice');
