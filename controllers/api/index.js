1b1ae4b7ad6f5fd62f64b906fb2a712260d7a555
Megan Steblay <megan@steblay.net>
2021-07-27 13:40:01 -0500

    Fixing signup and login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./userRoutes');
-const signupRoutes = require('./signupRoutes');
-const questionRoutes = require('./questionRoutes');
-const addQuestionRoutes = require('./addQuestionRoutes');
-const { route } = require('./userRoutes');
+const userRoutes = require("./userRoutes");
+const questionRoutes = require("./questionRoutes");
+const addQuestionRoutes = require("./addQuestionRoutes");
+const { route } = require("./userRoutes");
 
-router.use('/users', userRoutes);
-router.use('/signup', signupRoutes);
-router.use('/question', questionRoutes);
-router.use('/addQuestion', addQuestionRoutes);
+router.use("/users", userRoutes);
+router.use("/question", questionRoutes);
+router.use("/addQuestion", addQuestionRoutes);
 
 module.exports = router;
