991cb88649f2746ce8cc496f24d3689ec049de3c
Hope <johnhope354@gmail.com>
2021-07-27 22:46:34 -0500

    shuffled questions, updates to move right when correct answer selected

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const userRoutes = require("./userRoutes");
-const questionRoutes = require("./questionRoutes");
-const addQuestionRoutes = require("./addQuestionRoutes");
-const { route } = require("./userRoutes");
+const userRoutes = require('./userRoutes');
+//const signupRoutes = require('./signupRoutes');
+const questionRoutes = require('./questionRoutes');
+const addQuestionRoutes = require('./addQuestionRoutes');
+const { route } = require('./userRoutes');
 
-router.use("/users", userRoutes);
-router.use("/question", questionRoutes);
-router.use("/addQuestion", addQuestionRoutes);
+router.use('/users', userRoutes);
+//router.use('/signup', signupRoutes);
+router.use('/question', questionRoutes);
+router.use('/addquestion', addQuestionRoutes);
 
 module.exports = router;
