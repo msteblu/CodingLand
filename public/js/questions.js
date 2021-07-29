14e7edd5aa9810fa8ae2dd3750bae8c4b2379f0f
Amira Holden <amiraholden@gmail.com>
2021-07-29 14:15:41 -0500

    spin char; correct ans disappears; delay before modal

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,18 +1,26 @@
 console.log("loading questions js ...");
 
-let additive =80;
+let additive = 80;
 let modal = document.querySelector('.openModal')
 
-document.querySelector(".question-container").addEventListener("click",(event) => {
+document.querySelector(".question-container").addEventListener("click", (event) => {
     if (event.target.classList.contains("question-btn")) {
-        const isCorrect = event.target.getAttribute("correct") === "true" ? true: false;
-    
-        console.log("isCorrect",isCorrect)
+        const isCorrect = event.target.getAttribute("correct") === "true" ? true : false;
+        // added event.target.parentElement display=none below
+        console.log("isCorrect", isCorrect)
         if (isCorrect) {
             moveRight(additive);
-            additive+=80;
+            // resetRotation();
+            additive += 80;
+            event.target.parentElement.style.display = "none";
             if (additive === 480) {
-                modal.style.display = "flex";
+                // adding delay and timer function
+                const delay = 5000;
+                setTimeout(function () {
+                        modal.style.display = "flex";
+                    },
+                    delay);
+
             }
         } else {
             alert("Wrong!");
@@ -22,8 +30,8 @@ document.querySelector(".question-container").addEventListener("click",(event) =
 
 replay = () => {
     document.location.replace("/questions")
-  }
+}
 
-  noReplay = () => {
+noReplay = () => {
     document.location.replace("/")
-  }
+}
